057cd85dfc362c6a18116ad7e80acb77fa794617
Ernesto Custodio <ernestojosecg@gmail.com>
2020-07-20 12:00:36 +0200

    Vue inst

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -3,14 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <h1>Chat Room</h1>
+        <div class="row" id="app">
+            <div class="list-group offset-4 col-4">
+                <a href="#" class="list-group-item list-group-item-action active">
+                  Chat Room
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+                <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+                <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+                <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Vestibulum at eros</a>
+                <input type="text" class="form-control" placeholder="Type your message">
+              </div>
         </div>
     </div>
     
